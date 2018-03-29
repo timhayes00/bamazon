8f44d4f25e1bbe6ea081652b085af4adc3fb18c2
timhayes00 <timhayes00@gmail.com>
2018-03-29 01:21:51 -0400

    really, last upload tonight

--- bamazon.js
+++ bamazon.js
@@ -32,5 +32,32 @@ function displayItems(){
             console.log("There are " + res[i].stock_quantity + " left in stock.");
             console.log("=================================");
         }
+        purchaseItems();
     })
 }
+
+function purchaseItems(){
+    inquirer
+    .prompt([
+        {
+         name: "item_id",
+         type: "input",
+         message: "Please enter the Item ID of what you would like to buy today.",
+        },
+        {
+         name: "quantity",
+         type: "input",
+         message: "How many would you like to purchase?",
+        }
+    ])
+    .then(function(answer){
+        console.log(answer);
+        var query = "SELECT item_id, stock_quantity FROM products WHERE ?";
+        connection.query(query, [answer.item_id, answer.stock_quantity], function (err, res){
+            if (err) throw err;
+            
+
+        })
+    })
+}
+
